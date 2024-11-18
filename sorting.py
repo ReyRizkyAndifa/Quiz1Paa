84dcbfb6aa0301986d65cb5360fec8986b6b970f
ReyRizkyAndifa <153422590+ReyRizkyAndifa@users.noreply.github.com>
2024-11-18 15:26:00 +0800

    Add files via upload

---
+++ sorting.py
@@ -0,0 +1,55 @@
+import time
+
+def pengurutan_bubble(data):
+    n = len(data)
+    for i in range(n - 1):
+        for j in range(n - i - 1):
+            if data[j] > data[j + 1]:  
+                data[j], data[j + 1] = data[j + 1], data[j]
+    return data
+
+
+def pengurutan_quick(data):
+    if len(data) <= 1:
+        return data
+    pivot = data[len(data) // 2]
+    kiri = [x for x in data if x < pivot]
+    tengah = [x for x in data if x == pivot]
+    kanan = [x for x in data if x > pivot]
+    return pengurutan_quick(kiri) + tengah + pengurutan_quick(kanan)
+
+
+def ukur_waktu(fungsi_pengurutan, data):
+    waktu_mulai = time.time()
+    data_terurut = fungsi_pengurutan(data)
+    waktu_selesai = time.time()
+    return data_terurut, waktu_selesai - waktu_mulai
+
+
+def analisis_efisiensi(label, waktu_bubble, waktu_quick):
+    if waktu_bubble < waktu_quick:
+        efisien = "Bubble Sort lebih efisien untuk " + label
+    elif waktu_quick < waktu_bubble:
+        efisien = "Quick Sort lebih efisien untuk " + label
+    else:
+        efisien = "Kedua metode sama efisien untuk " + label
+    return efisien
+
+
+kelas_a = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+kelas_b = [0, 1, 2, 3, 4, 8, 7, 6, 5]
+
+
+for label, data in [("Kelas A", kelas_a), ("Kelas B", kelas_b)]:
+    hasil_bubble, waktu_bubble = ukur_waktu(pengurutan_bubble, data[:])  # Salin data agar tidak terpengaruh
+    hasil_quick, waktu_quick = ukur_waktu(pengurutan_quick, data[:])    
+
+    
+    print(f"{label}:")
+    print(f"  Data Asli: {data}")
+    print(f"  Hasil Bubble Sort: {hasil_bubble}")
+    print(f"  Waktu Bubble Sort: {waktu_bubble:.8f} detik")
+    print(f"  Hasil Quick Sort: {hasil_quick}")
+    print(f"  Waktu Quick Sort: {waktu_quick:.8f} detik")
+    
+    print(f"  Analisis: {analisis_efisiensi(label, waktu_bubble, waktu_quick)}\n")
